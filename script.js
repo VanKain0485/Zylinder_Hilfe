bb286036882c71a610abcdd4997f1a690aed4f55
VanKain0485 <Dominik.Armer@gmx.de>
2026-03-03 12:40:28 +0100

    Ersetze Erwartungen durch Lösung im Selbstcheck

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ const showSolutionButton = document.getElementById('showSolution');
 const solution = document.getElementById('solution');
 showSolutionButton.addEventListener('click', () => {
   solution.classList.toggle('hidden');
-  showSolutionButton.textContent = solution.classList.contains('hidden') ? 'Erwartungen einblenden' : 'Erwartungen ausblenden';
+  showSolutionButton.textContent = solution.classList.contains('hidden') ? 'Lösung einblenden' : 'Lösung ausblenden';
 });
 
 function buildCommonScene(container, overlay) {
